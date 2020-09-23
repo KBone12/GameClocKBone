e1d3d067ce74398973618ef00c2b351a8a9b242e
kbone <kbonehobby@gmail.com>
2020-09-23 21:21:40 +0900

    Show the time

--- src/pane/clock.rs
+++ src/pane/clock.rs
@@ -1,16 +1,28 @@
-use iced::{Column, Element};
+use iced::{Column, Element, Text};
+use std::time::Duration;
 
-pub struct Clock1PPane {}
+pub struct Clock1PPane {
+    remaining: Duration,
+}
 
 impl Clock1PPane {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            remaining: Duration::new(0, 0),
+        }
     }
 
     pub fn update(&mut self, _message: Clock1PMessage) {}
 
     pub fn view(&mut self) -> Element<Clock1PMessage> {
-        Column::new().into()
+        let sec = self.remaining.as_secs();
+        let hour = sec / (60 * 60);
+        let sec = sec - hour * (60 * 60);
+        let min = sec / 60;
+        let sec = sec - min * 60;
+        Column::new()
+            .push(Text::new(format!("{:02}:{:02}:{:02}", hour, min, sec)))
+            .into()
     }
 }
 
